cc90ffc755e57414c3912afa2eb8e293f0d6e2e5
littlejie <1025263614lsj@gmail.com>
2017-12-01 10:27:21 +0800

    FirstService IntentService版

--- app/src/main/java/com/apkbus/servicedemo/service/FirstService.kt
+++ app/src/main/java/com/apkbus/servicedemo/service/FirstService.kt
@@ -1,17 +1,17 @@
 package com.apkbus.servicedemo.service
 
 import android.annotation.SuppressLint
-import android.app.Service
+import android.app.IntentService
 import android.content.Intent
-import android.os.*
+import android.os.Handler
+import android.os.Message
 import android.util.Log
 import android.widget.Toast
 
-class FirstService : Service() {
+//IntentService构造方法需要有name参数，用于设置子线程的名称，方便调试
+//建议传类名
+class FirstService : IntentService(TAG) {
 
-    //用于处理耗时操作的Handler
-    private var mServiceHandler: Handler? = null
-    private var mServiceThread: HandlerThread? = null
     //主线程上的Handler，用于更新UI
     @SuppressLint("HandlerLeak")
     private val mMainHandler = object : Handler() {
@@ -19,66 +19,36 @@ class FirstService : Service() {
         override fun handleMessage(msg: Message) {
             super.handleMessage(msg)
             when (msg.what) {
-                MESSAGE_SLEEP -> {
-                    Log.d(TAG, "当前线程ID：" + Thread.currentThread().id)
-                    Toast.makeText(applicationContext, "让我先睡20s", Toast.LENGTH_SHORT).show()
-                }
-                MESSAGE_GET_UP -> {
-                    Toast.makeText(applicationContext, "我睡醒了~服务关闭", Toast.LENGTH_SHORT).show()
-                    //后台操作结束，主动关闭
-                    //之后会回调onDestroy()
-                    stopSelf()
-                }
+                MESSAGE_SLEEP -> Toast.makeText(applicationContext, "让我先睡20s", Toast.LENGTH_SHORT).show()
+                MESSAGE_GET_UP -> Toast.makeText(applicationContext, "我睡醒了~服务关闭", Toast.LENGTH_SHORT).show()
             }
         }
     }
 
-    override fun onBind(intent: Intent): IBinder? {
-        Log.d(TAG, TAG + " onBind")
-        // TODO: Return the communication channel to the service.
-        throw UnsupportedOperationException("Not yet implemented")
-    }
+    override fun onHandleIntent(intent: Intent?) {
+        Log.d(TAG, "onHandleIntent, 当前线程ID：" + Thread.currentThread().id)
+        //注意，onHandleIntent 是在子线程中执行，所以请不要在此更新UI
+        mMainHandler.sendEmptyMessage(MESSAGE_SLEEP)
+        val startTime = System.currentTimeMillis()
+        //模拟耗时操作
+        try {
+            Thread.sleep(20000)
+        } catch (e: InterruptedException) {
+            e.printStackTrace()
+        }
 
-    override fun onUnbind(intent: Intent): Boolean {
-        Log.d(TAG, TAG + " onUnbind")
-        return super.onUnbind(intent)
+        Log.d(TAG, "耗时操作已结束，用时：" + (System.currentTimeMillis() - startTime) / 1000 + "s，该服务即将关闭")
+        mMainHandler.sendEmptyMessage(MESSAGE_GET_UP)
     }
 
     override fun onCreate() {
         super.onCreate()
         Log.d(TAG, TAG + " onCreate")
-        mServiceThread = HandlerThread("FirstService", Process.THREAD_PRIORITY_BACKGROUND)
-        mServiceThread!!.start()
-
-        val serviceLooper = mServiceThread!!.looper
-        mServiceHandler = ServiceHandler(serviceLooper)
     }
 
-    override fun onStartCommand(intent: Intent, flags: Int, startId: Int): Int {
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
         Log.d(TAG, TAG + " onStartCommand")
-        mServiceHandler!!.sendEmptyMessage(MESSAGE_SLEEP)
         return super.onStartCommand(intent, flags, startId)
-    }
-
-    private inner class ServiceHandler(looper: Looper) : Handler(looper) {
-
-        override fun handleMessage(msg: Message) {
-            super.handleMessage(msg)
-            Log.d(TAG, "当前线程ID：" + Thread.currentThread().id + "；即将开始耗时操作")
-            //发送消息提示主线程弹出Toast
-            mMainHandler.sendEmptyMessage(MESSAGE_SLEEP)
-            val startTime = System.currentTimeMillis()
-            //模拟耗时操作
-            try {
-                Thread.sleep(20000)
-            } catch (e: InterruptedException) {
-                e.printStackTrace()
-            }
-
-            //发送消息提示主线程耗时操作已结束，弹出Toast
-            mMainHandler.sendEmptyMessage(MESSAGE_GET_UP)
-            Log.d(TAG, "耗时操作已结束，用时：" + (System.currentTimeMillis() - startTime) / 1000 + "s，该服务即将关闭")
-        }
     }
 
     override fun onDestroy() {
@@ -92,5 +62,4 @@ class FirstService : Service() {
         private val MESSAGE_SLEEP = 1
         private val MESSAGE_GET_UP = 2
     }
-
 }
